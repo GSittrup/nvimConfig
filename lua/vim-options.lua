d1ad5dd4ddbcc9952549029ce4d3bbb6a02faef9
GSittrup <glenn.s.olesen@gmail.com>
2024-11-14 22:37:04 +0100

    tl;dr

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -13,11 +13,11 @@ vim.opt.relativenumber = true
 -- Number of lines to keep above and below cursor
 vim.opt.scrolloff = 10
 
--- Dont show mode, its already in the status line 
+-- Dont show mode, its already in the status line
 vim.opt.showmode = false
 
--- Get used to it boy! 
-vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+-- Get used to it boy!
+vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
